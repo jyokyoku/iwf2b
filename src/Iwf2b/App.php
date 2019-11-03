d40ff6b487b96c0bb0bef2600efa67449b4570ff
jyokyoku <jyokyoku@gmail.com>
2019-11-03 17:39:26 +0900

    modify: App::bootstrap - the directory and namespace array can be used to an args.

--- src/Iwf2b/App.php
+++ src/Iwf2b/App.php
@@ -35,45 +35,62 @@ class App {
 	}
 
 	/**
-	 * Create all instances of AbstractSingleton sub classes.
+	 * @param array|string $base_dir
+	 * @param string $base_namespace
 	 */
-	public function bootstrap( $base_dir, $base_namespace ) {
-		$recursive_include = function ( $parent_dir, $namespace ) use ( &$recursive_include ) {
-			foreach ( scandir( $parent_dir ) as $file_name ) {
-				if ( $file_name === '.' || $file_name === '..' ) {
+	public function bootstrap( $base_directory, $base_namespace = null ) {
+		if ( is_array( $base_directory ) && $base_namespace === null ) {
+			$class_maps = $base_directory;
+
+		} else if ( is_string( $base_directory ) && ! empty( $base_namespace ) ) {
+			$class_maps = [ $base_directory => $base_namespace ];
+
+		} else {
+			throw new \InvalidArgumentException( sprintf( "The \$base_directroy must be an array or string and set the \$base_namespace if \$base_directory is a string." ) );
+		}
+
+		foreach ( $class_maps as $_base_directory => $_base_namespace ) {
+			$this->load_recursive( $_base_directory, $_base_namespace );
+		}
+	}
+
+	/**
+	 * @param string $parent_directory
+	 * @param string $namespace
+	 */
+	private function load_recursive( $parent_dir, $namespace ) {
+		foreach ( scandir( $parent_dir ) as $file_name ) {
+			if ( $file_name === '.' || $file_name === '..' ) {
+				continue;
+			}
+
+			$file_path = $parent_dir . '/' . $file_name;
+
+			if ( is_file( $file_path ) && strrpos( $file_name, '.php' ) !== false && ctype_upper( substr( $file_name, 0, 1 ) ) && $file_path !== __FILE__ ) {
+				$class = substr( $file_name, 0, - 4 );
+
+				if ( substr( $class, 0, 8 ) === 'Abstract' || substr( $class, - 9 ) === 'Interface' || substr( $class, - 6 ) === 'Trait' ) {
 					continue;
 				}
 
-				$file_path = $parent_dir . '/' . $file_name;
+				$namespace_class = $namespace . '\\' . substr( $file_name, 0, - 4 );
 
-				if ( is_file( $file_path ) && strrpos( $file_name, '.php' ) !== false && ctype_upper( substr( $file_name, 0, 1 ) ) && $file_path !== __FILE__ ) {
-					$class = substr( $file_name, 0, - 4 );
+				try {
+					$ref = new \ReflectionClass( $namespace_class );
 
-					if ( substr( $class, 0, 8 ) === 'Abstract' || substr( $class, - 9 ) === 'Interface' || substr( $class, - 6 ) === 'Trait' ) {
-						continue;
-					}
-
-					$namespace_class = $namespace . '\\' . substr( $file_name, 0, - 4 );
-
-					try {
-						$ref = new \ReflectionClass( $namespace_class );
-
-					} catch ( \ReflectionException $e ) {
-						continue;
-					}
-
-					if ( $ref->isSubclassOf( __NAMESPACE__ . '\AbstractSingleton' ) ) {
-						if ( $namespace_class::auto_init() ) {
-							$namespace_class::get_instance();
-						}
-					}
-
-				} else if ( is_dir( $file_path ) ) {
-					$recursive_include( $file_path, $namespace . '\\' . basename( $file_path ) );
+				} catch ( \ReflectionException $e ) {
+					continue;
 				}
-			}
-		};
 
-		$recursive_include( $base_dir, $base_namespace );
+				if ( $ref->isSubclassOf( __NAMESPACE__ . '\AbstractSingleton' ) ) {
+					if ( $namespace_class::auto_init() ) {
+						$namespace_class::get_instance();
+					}
+				}
+
+			} else if ( is_dir( $file_path ) ) {
+				$this->load_recursive( $file_path, $namespace . '\\' . basename( $file_path ) );
+			}
+		}
 	}
 }
