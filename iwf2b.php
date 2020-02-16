3aae76f33eba7b921bdd99fb83de2e94aaff7ac2
jyokyoku <jyokyoku@gmail.com>
2020-02-17 01:47:34 +0900

    add: WordPress plugin base files

---
+++ iwf2b.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Iwf2b
+ * Plugin URI:      https://github.com/jyokyoku/iwf2b
+ * Description:     Library for WordPress development
+ * Author:          jyokyoku
+ * Author URI:      https://devlog.to
+ * Text Domain:     iwf2b
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Iwf2b
+ */
+
+require_once 'vendor/autoload.php';
