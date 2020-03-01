4b8e019cc2105fbcf13694e6da94d60f0d74671f
jyokyoku <jyokyoku@gmail.com>
2020-03-02 00:08:51 +0900

    Refactoring

--- tests/TestCase/Post/AbstractPostTest.php
+++ tests/TestCase/Post/AbstractPostTest.php
@@ -5,31 +5,6 @@ namespace Iwf2b\Tests\TestCase\Post;
 use Iwf2b\Post\AbstractPost;
 
 class AbstractPostTest extends \WP_UnitTestCase {
-	protected function set_static_property( $class, $prop, $value ) {
-		$ref  = new \ReflectionClass( $class );
-
-		$prop = $ref->getProperty( $prop );
-		$prop->setAccessible( true );
-		$prop->setValue( $value );
-	}
-
-	protected function make_post_class( $class, $post_type, array $args = [], $register = false ) {
-		$post_class = \Mockery::namedMock( $class, AbstractPost::class );
-		$post_class->makePartial();
-
-		$this->set_static_property( $post_class, 'post_type', $post_type );
-
-		if ( $args ) {
-			$this->set_static_property( $post_class, 'args', $args );
-		}
-
-		if ( $register ) {
-			$post_class->register_post_type();
-		}
-
-		return $post_class;
-	}
-
 	public function tearDown() {
 		parent::tearDown();
 
@@ -76,5 +51,48 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		], true );
 
 		$this->assertEquals( get_post_type_object( 'test_post' ), $post_class::get_post_type_object() );
+	}
+
+	public function test_get_thumbnail() {
+		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
+			'supports'    => [ 'title', 'editor' ],
+			'has_archive' => true,
+		], true );
+
+		$post_id = $this->factory->post->create( [
+			'post_type'  => 'test_post',
+			'post_title' => 'Test',
+		] );
+
+		$attachment_id = $this->factory->attachment->create_upload_object( 'test.jpeg', $post_id );
+		set_post_thumbnail( $post_id, $attachment_id );
+
+		$thumbnail = $post_class::get_thumbnail( $post_id );
+		var_dump( $thumbnail );
+	}
+
+	protected function set_static_property( $class, $prop, $value ) {
+		$ref = new \ReflectionClass( $class );
+
+		$prop = $ref->getProperty( $prop );
+		$prop->setAccessible( true );
+		$prop->setValue( $value );
+	}
+
+	protected function make_post_class( $class, $post_type, array $args = [], $register = false ) {
+		$post_class = \Mockery::namedMock( $class, AbstractPost::class );
+		$post_class->makePartial();
+
+		$this->set_static_property( $post_class, 'post_type', $post_type );
+
+		if ( $args ) {
+			$this->set_static_property( $post_class, 'args', $args );
+		}
+
+		if ( $register ) {
+			$post_class->register_post_type();
+		}
+
+		return $post_class;
 	}
 }
