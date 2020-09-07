a95746175863a0b078021d5cd9debf1fb0b4bcb0
jyokyoku <jyokyoku@gmail.com>
2020-09-07 10:41:28 +0900

    modify: Remove Mockry from tests

--- tests/TestCase/Post/AbstractPostTest.php
+++ tests/TestCase/Post/AbstractPostTest.php
@@ -18,76 +18,46 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		$this->root = vfsStream::setup();
 	}
 
-	public function tearDown() {
-		parent::tearDown();
-
-		\Mockery::close();
-	}
-
 	public function test_get() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [], true );
-
 		$post_id = $this->factory->post->create( [
 			'post_type'  => 'test_post',
 			'post_title' => 'Test',
 		] );
 
-		$this->assertEmpty( $post_class::get( 0 ) );
-		$this->assertEquals( $post_class::get( $post_id ), get_post( $post_id ) );
+		$this->assertEmpty( TestPost::get( 0 ) );
+		$this->assertEquals( TestPost::get( $post_id ), get_post( $post_id ) );
 	}
 
 	public function test_is_valid() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [], true );
-
 		$post_id = $this->factory->post->create( [
 			'post_type'  => 'test_post',
 			'post_title' => 'Test',
 		] );
 
-		$this->assertFalse( $post_class::is_valid( 0 ) );
-		$this->assertTrue( $post_class::is_valid( $post_id ) );
+		$this->assertFalse( TestPost::is_valid( 0 ) );
+		$this->assertTrue( TestPost::is_valid( $post_id ) );
 	}
 
 	public function test_get_archive_link() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
-		$this->assertEquals( get_post_type_archive_link( 'test_post' ), $post_class::get_archive_link() );
+		$this->assertEquals( get_post_type_archive_link( 'test_post' ), TestPost::get_archive_link() );
 	}
 
 	public function test_get_post_type_object() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
-		$this->assertEquals( get_post_type_object( 'test_post' ), $post_class::get_post_type_object() );
+		$this->assertEquals( get_post_type_object( 'test_post' ), TestPost::get_post_type_object() );
 	}
 
 	public function test_get_thumbnail() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
 		$post_id = $this->factory->post->create( [
 			'post_type'  => 'test_post',
 			'post_title' => 'Test',
 		] );
 
-		$thumbnail = $post_class::get_thumbnail( $post_id );
+		$thumbnail = TestPost::get_thumbnail( $post_id );
 
 		$this->assertEmpty( $thumbnail );
 	}
 
 	public function test_get_thumbnail_from_featured_image() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
 		$post_id = $this->factory->post->create( [
 			'post_type'  => 'test_post',
 			'post_title' => 'Test',
@@ -99,7 +69,7 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		set_post_thumbnail( $post_id, $attachment_id );
 
 		// Test featured image
-		$thumbnail = $post_class::get_thumbnail( $post_id );
+		$thumbnail = TestPost::get_thumbnail( $post_id );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 200, $thumbnail['width'] );
@@ -107,7 +77,7 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		$this->assertEquals( 'Image alt text', $thumbnail['alt'] );
 
 		// Test overwrite alt keyword
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'alt' => 'Overwrite alt text' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'alt' => 'Overwrite alt text' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 200, $thumbnail['width'] );
@@ -119,11 +89,6 @@ class AbstractPostTest extends \WP_UnitTestCase {
 	 * @depends test_get_thumbnail_from_featured_image
 	 */
 	public function test_get_thumbnail_from_dummy_image() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
 		$post_id = $this->factory->post->create( [
 			'post_type'  => 'test_post',
 			'post_title' => 'Dummy image test',
@@ -132,12 +97,12 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		$test_img_path = $this->create_virtual_image( 'test.gif', 'gif', [ 'width' => 320, 'height' => 240 ] );
 
 		// Test empty if featured image is empty
-		$thumbnail = $post_class::get_thumbnail( $post_id );
+		$thumbnail = TestPost::get_thumbnail( $post_id );
 
 		$this->assertEmpty( $thumbnail );
 
 		// Test dummy image
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => '' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => '' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 320, $thumbnail['width'] );
@@ -145,7 +110,7 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		$this->assertEquals( '', $thumbnail['alt'] );
 
 		// Test overwrite alt keyword
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => 'Image alt text' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => 'Image alt text' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 320, $thumbnail['width'] );
@@ -158,7 +123,7 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		update_post_meta( $attachment_id, '_wp_attachment_image_alt', 'Image alt text 2' );
 		set_post_thumbnail( $post_id, $attachment_id );
 
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => '' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'dummy_image' => $test_img_path, 'alt' => '' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 180, $thumbnail['width'] );
@@ -167,11 +132,6 @@ class AbstractPostTest extends \WP_UnitTestCase {
 	}
 
 	public function test_get_thumbnail_from_first_image_in_post_content() {
-		$post_class = $this->make_post_class( 'TestPost', 'test_post', [
-			'supports'    => [ 'title', 'editor' ],
-			'has_archive' => true,
-		], true );
-
 		$test_img_path = $this->create_virtual_image( 'test.png', 'png', [ 'width' => 280, 'height' => 190 ] );
 
 		kses_remove_filters();  // Allow 'vfs' protocol to the post_content
@@ -183,12 +143,12 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		] );
 
 		// Test invalid keyword of post object
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'search_post_key' => 'dummy_keyword' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'search_post_key' => 'dummy_keyword' ] );
 
 		$this->assertEmpty( $thumbnail );
 
 		// Test post_content
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'search_post_key' => 'post_content' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'search_post_key' => 'post_content' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 280, $thumbnail['width'] );
@@ -196,37 +156,12 @@ class AbstractPostTest extends \WP_UnitTestCase {
 		$this->assertEquals( 'Image alt text', $thumbnail['alt'] );
 
 		// Test overwrite alt keyword
-		$thumbnail = $post_class::get_thumbnail( $post_id, [ 'search_post_key' => 'post_content', 'alt' => 'Overwrite alt text' ] );
+		$thumbnail = TestPost::get_thumbnail( $post_id, [ 'search_post_key' => 'post_content', 'alt' => 'Overwrite alt text' ] );
 
 		$this->assertNotEmpty( $thumbnail['src'] );
 		$this->assertEquals( 280, $thumbnail['width'] );
 		$this->assertEquals( 190, $thumbnail['height'] );
 		$this->assertEquals( 'Overwrite alt text', $thumbnail['alt'] );
-	}
-
-	protected function set_static_property( $class, $prop, $value ) {
-		$ref = new \ReflectionClass( $class );
-
-		$prop = $ref->getProperty( $prop );
-		$prop->setAccessible( true );
-		$prop->setValue( $value );
-	}
-
-	protected function make_post_class( $class, $post_type, array $args = [], $register = false ) {
-		$post_class = \Mockery::namedMock( $class, AbstractPost::class );
-		$post_class->makePartial();
-
-		$this->set_static_property( $post_class, 'post_type', $post_type );
-
-		if ( $args ) {
-			$this->set_static_property( $post_class, 'args', $args );
-		}
-
-		if ( $register ) {
-			$post_class->register_post_type();
-		}
-
-		return $post_class;
 	}
 
 	protected function create_virtual_image( $file_name, $type = 'jpg', array $args = [] ) {
@@ -256,4 +191,13 @@ class AbstractPostTest extends \WP_UnitTestCase {
 
 		return vfsStream::newFile( $file_name )->at( $this->root )->setContent( $image_data )->url();
 	}
+}
+
+class TestPost extends AbstractPost {
+	protected static $post_type = 'test_post';
+
+	protected static $args = [
+		'has_archive' => true,
+		'supports' => ['title', 'editor']
+	];
 }
